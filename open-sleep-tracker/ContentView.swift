70b85d294bb577f5515e8433b1a1608620410929
saikiran-chatti <jayanthsaikiran@gmail.com>
2025-10-16 02:21:21 -0400

    Initial Commit

---
+++ open-sleep-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  open-sleep-tracker
+//
+//  Created by Jay Chatti on 10/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ open-sleep-tracker/open_sleep_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_sleep_trackerApp.swift
+//  open-sleep-tracker
+//
+//  Created by Jay Chatti on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct open_sleep_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
